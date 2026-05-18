54100e3e8213c72f6d98e313a367138d5a7deeab
SUYASH SINHA <suyash.sinha.104@gmail.com>
2026-05-19 04:19:58 +0530

    Updated CORS for production

--- src/main/java/com/atomquest/goalportal/controller/GoalController.java
+++ src/main/java/com/atomquest/goalportal/controller/GoalController.java
@@ -19,7 +19,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","https://YOUR-VERCEL-URL.vercel.app"})
 @RequestMapping("/goals")
 
 public class GoalController {
